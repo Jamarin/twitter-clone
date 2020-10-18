df14058d14ca3ddaa926301e295451006c7a54eb
joseantoniomarin <joseantonio.marin@tellmegen.com>
2020-10-18 18:05:06 +0200

    [ADD] Prepare first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+import VueRouter from 'vue-router'
+import {router} from '@/router'
+
+import store from '@/store'
+
+Vue.use(Buefy)
+Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router,
+    store
 }).$mount('#app')
